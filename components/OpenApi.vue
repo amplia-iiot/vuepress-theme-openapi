83b672fd0bf33dcccf9083671657aeeafca23f53
Victor Gutierrez <gtrrz.victor@gmail.com>
2020-11-26 12:46:55 +1100

    add button to download the current specification in JSON format

--- components/OpenApi.vue
+++ components/OpenApi.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="open-api-container"></div>
+  <div>
+    <div class="download-spec" @click="download">Download Spec</div>
+    <div :class="baseClass"></div>
+  </div>
 </template>
 
 <script>
+import { saveAs } from 'file-saver'
 import SwaggerUI from 'swagger-ui'
 import 'swagger-ui/dist/swagger-ui.css'
 export default {
@@ -20,6 +24,13 @@ export default {
       }
     }
   },
+  data(){
+    return {
+      baseClass:'open-api-container',
+      finalSpec:null,
+      finalSpecName:null,
+    }
+  },
   computed: {
     isYaml () {
       return this.$frontmatter.openapi === 'yaml'
@@ -32,16 +43,21 @@ export default {
     this.loadSwagger()
   },
   methods: {
+    download(){
+      const blob = new Blob([JSON.stringify(this.finalSpec, null, 4)], {type: "text/plain;charset=utf-8"});
+      saveAs(blob, `${this.finalSpecName}.json`);
+    },
     async loadSwagger(){
       const { servers = [], commonSchemas = [] } = this.$themeConfig;
       const composedNameSpec = this.converPagePathToSpecFilename()
       try{
         const spec = await import(`@specs/${composedNameSpec}.${this.extension}`)
         const commonSchemasContent = await this.findCommonSchemas(commonSchemas)
-        this.mixCommonSchemas(spec,commonSchemasContent)
+        this.finalSpec = this.mixCommonSchemas(spec,commonSchemasContent)
+        this.finalSpecName = composedNameSpec
         SwaggerUI({
           spec: { ...spec, servers: servers.map(url => ({ url })) },
-          domNode: this.$el,
+          domNode: this.$el.querySelector(`.${this.baseClass}`),
         })
       }catch(err){
         this.$el.innerHTML = ''
@@ -68,6 +84,7 @@ export default {
           schemas:commonSchemas
         }
       }
+      return spec;
     }
   }
 }
@@ -76,6 +93,29 @@ export default {
 <style scope>
 .open-api-container pre {
   background-color: rgb(125, 132, 146);
+}
+
+.download-spec {
+  position: absolute;
+  right: 20px;
+  font-weight: 500;
+  display:inline-block;
+  border:1px solid #CCC;
+  box-shadow: 0 0 5px -1px rgba(0,0,0,0.2);
+  cursor:pointer;
+  vertical-align:middle;
+  max-width: 100px;
+  padding: 5px;
+  text-align: center;
+}
+
+.download-spec:hover {
+    box-shadow: 0 0 5px -1px rgba(0,0,0,0.6);
+}
+
+.download-spec:active {
+    color:red;
+    box-shadow: 0 0 5px -1px rgba(0,0,0,0.6);
 }
 </style>
 
